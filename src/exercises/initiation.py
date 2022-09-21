9f237131ad690630a5accc0a95d403b34bc40496
tms-gvd <thomas.gaviard@gmail.com>
2022-09-21 14:37:34 +0200

    feats: added exercise 'Non-unique Elements'

--- src/exercises/initiation.py
+++ src/exercises/initiation.py
@@ -1,2 +1,23 @@
 def mult_two(a: int, b: int) -> int:
+    """https://py.checkio.org/en/mission/multiply-intro/
+
+    Args:
+        a (int): first input
+        b (int): second input
+
+    Returns:
+        int: multiplication of a and b.
+    """
     return a*b
+
+
+def checkio(data: list) -> list:
+    """https://py.checkio.org/en/mission/non-unique-elements/
+
+    Args:
+        data (list): list to check
+
+    Returns:
+        list: list consisting of only the non-unique elements in the input list. 
+    """
+    return [val for val in data if data.count(val)>1]
--- tests/test_initiation.py
+++ tests/test_initiation.py
@@ -1,6 +1,19 @@
-from exercises.initiation import mult_two
+from exercises.initiation import mult_two, checkio
 import pytest
 
 def test_mult_two():
     assert mult_two(3, 2) == 6
     assert mult_two(0, 1) == 0
+
+def test_checkio():
+    assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3]
+    assert checkio([1, 2, 3, 4, 5]) == []
+    assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5]
+    assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9]
+    assert checkio([2, 2, 3, 2, 2]) == [2, 2, 2, 2]
+    assert checkio([10, 20, 30, 10]) == [10, 10]
+    assert checkio([7]) == []
+    assert checkio([0, 1, 2, 3, 4, 0, 1, 2, 4]) == [0, 1, 2, 4, 0, 1, 2, 4]
+    assert checkio([99, 98, 99]) == [99, 99]
+    assert checkio([0, 0, 0, 1, 1, 100]) == [0, 0, 0, 1, 1]
+    assert checkio([0, 0, 0, -1, -1, 100]) == [0, 0, 0, -1, -1]
